01f77602dc90e2b292d0e3c352475856e04f3cc0
ismoilov110 <behruzismoilov64@gmail.com>
2025-11-19 18:13:57 +0500

    gg

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from './_components/header'
 import ShortlyAbout from './_components/shortlyAbout'
+import Freetrial from './_components/Freetrial'
 
 export default function page() {
     return (
         <div>
             <Header />
             <ShortlyAbout />
+            <Freetrial/>
         </div>
     )
 }
