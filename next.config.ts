01c0293bc928fc86346db74f328722ae7d5d8042
Muhammad-amin7 <abduhalilovshohruh681@gmail.com>
2025-11-29 17:54:55 +0500

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,8 @@
-// next.config.js
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        // Rasmlar serverining protokoli
-        protocol: 'http', 
-        // Rasmlar serverining IP manzili
-        hostname: '192.168.1.52', 
-        // Rasmlar serverining porti
-        port: '8000', 
-        // Rasmlar joylashgan yo'l (ixtiyoriy, lekin tavsiya etiladi)
-        pathname: '/media/**', 
-      },
-    ],
+    domains: ["fintechhub.ziyodev.uz"], // shu yerga domenni qo‘shasiz
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
