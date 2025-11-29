3b1dc01312009d774832283e61c03c5ff7dbab40
Muhammad-amin7 <abduhalilovshohruh681@gmail.com>
2025-11-29 19:14:42 +0500

    fix

--- components/BlogCard.tsx
+++ components/BlogCard.tsx
@@ -1,18 +1,16 @@
 "use client";
 
 import Image from "next/image";
-import image from '@/assets/images/benifits.jpg'
 import Link from "next/link";
-import { useGetBlogsQuery } from "@/lib/services/api";
 import { IBlogs } from "@/types/types";
 
 export default function BlogCard({ isBig, data }: { isBig: boolean, data: IBlogs }) {
     return (
         <div>
-            <Link href={`blog/${data.title}-${data.id}`}>
+            <Link href={`blog/${data?.title}-${data?.id}`}>
                 <div className="lg:hover:-translate-y-3 duration-100 group">
                     <div className="relative">
-                        <Image src={data.image} alt="Fintechhub blog" width={4000} height={2000} className="w-full max-h-[217px] h-full object-cover" />
+                        <Image src={data?.image} alt="Fintechhub blog" width={4000} height={2000} className="w-full max-h-[217px] h-full object-cover" />
 
                         <div className="w-full h-full absolute top-0 left-0 bg-black/20 items-center justify-center text-white opacity-0 lg:group-hover:opacity-100 flex duration-200 font-medium text-sm cursor-pointer backdrop-blur-sm">
                             batafsil korish uchun bosing
@@ -20,7 +18,7 @@ export default function BlogCard({ isBig, data }: { isBig: boolean, data: IBlogs
                     </div>
 
                     <div className="py-2 px-1">
-                        <h3 className="text-sm font-medium">{data.title}</h3>
+                        <h3 className="text-sm font-medium">{data?.title}</h3>
                         <p className="text-neutral-600 text-xs leading-4 lg:hidden">
                             {data?.description?.slice(0, 180)}
                         </p>
