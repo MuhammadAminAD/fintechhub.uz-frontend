4c141b8cc9b30e7a88c459a1b9b594e314a22a95
Muhammad-amin7 <abduhalilovshohruh681@gmail.com>
2025-11-30 10:13:44 +0500

    updat blogs carousel

--- app/(root)/_components/blogsCarousel.tsx
+++ app/(root)/_components/blogsCarousel.tsx
@@ -1,22 +1,28 @@
+"use client"
+
 import BlogCard from "@/components/BlogCard"
 import {
     Carousel,
     CarouselContent,
     CarouselItem,
 } from "@/components/ui/carousel"
+import { useGetBlogsQuery } from "@/lib/services/api"
 import { Styles } from "@/styles/styles"
+import { IBlogs } from "@/types/types"
 
 export default function BlogsCarousel() {
+    const { data } = useGetBlogsQuery(" ")
+
     return (
         <div className={Styles.container}>
 
             <h1 className="text-xl md:text-3xl lg:text-5xl leading-snug font-bold text-[#012237] mb-10">Eng so`ngi yangiliklar</h1>
             <Carousel opts={{ loop: true, align: "center" }}>
                 <CarouselContent>
-                    <CarouselItem className="basis-8/10 md:basis-1/2 lg:basis-1/4"><BlogCard isBig={false} /></CarouselItem>
-                    <CarouselItem className="basis-8/10 md:basis-1/2 lg:basis-1/4"><BlogCard isBig={false} /></CarouselItem>
-                    <CarouselItem className="basis-8/10 md:basis-1/2 lg:basis-1/4"><BlogCard isBig={false} /></CarouselItem>
-                    <CarouselItem className="basis-8/10 md:basis-1/2 lg:basis-1/4"><BlogCard isBig={false} /></CarouselItem>
+                    {data?.map((data: IBlogs, index: number) =>
+                        <CarouselItem className="basis-8/10 md:basis-1/2 lg:basis-1/4" key={index}><BlogCard isBig={false} data={data} /></CarouselItem>
+                    )}
+
                 </CarouselContent>
             </Carousel>
         </div>
